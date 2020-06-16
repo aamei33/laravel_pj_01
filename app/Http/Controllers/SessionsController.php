ee2b473128e93d8cc6a442aa9e3a52d15f1a90ce
aamei33 <786873450@qq.com>
2020-06-16 13:41:50 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Auth;
+class SessionsController extends Controller
+{
+    //
+    public function create(){
+        return view('Sessions.create');
+    }
+
+    public function store(Request $request){
+        $credentials = $this->validate($request,[
+            'email'=>'required|email|max:255',
+            'password'=>'required'
+        ]);
+
+/*
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            // 该用户存在于数据库，且邮箱和密码相符合
+        }
+   */
+
+        if(Auth::attempt($credentials)){
+            //登录成功后的相关操作
+            session()->flash('success','欢迎回来！');
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            //登录错误的相关操作
+            session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+
+        }
+
+        return;
+
+    }
+    public function destroy(){
+
+    }
+}
