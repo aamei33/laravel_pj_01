4bb182708412bac46e83dee268c9889b9c015f92
aamei33 <786873450@qq.com>
2020-06-16 19:12:42 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,7 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function statuses(){
+        return $this->hasMany(Status::class);
+    }
 }
