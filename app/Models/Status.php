f5a9ae734f34c5084e362ac67f2c33fb1dc39a49
aamei33 <786873450@qq.com>
2020-06-17 09:54:46 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     //
+    //定义 content 为允许用户更新的字段
+    protected $fillable = ['content'];
     public function user(){
         return $this->belongsTo(User::class);
     }
