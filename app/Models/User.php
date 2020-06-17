c547ed8f3015f44a3829cc1ba8d74f96569a7f12
aamei33 <786873450@qq.com>
2020-06-17 11:20:45 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,44 @@ class User extends Authenticatable
             ->orderBy('created_at','desc');
     }
 
+
+    // 可以通过  $user->followers();来获取粉丝列表  例如查看user_id = 1 查看 follower_id列表 即第一个用户的粉丝
+    public function followers($user_ids){
+
+        return $this->belongsToMany(User::CLASS,'followers','user_id', 'follower_id');
+    }
+
+    //通过 $user->followings() 来获取关注人数列表 例如查看 follower_id =1 即查看第一个用户关注的用户
+    public function followings(){
+        return $this->belongsToMany(User::Class,'followers','follower_id','user_id');
+    }
+
+
+    //is_array 用于判断参数是否为数组，如果已经是数组，则没有必要再使用 compact 方法。
+    //我们并没有给 sync 和 detach 指定传递参数为用户的 id，这两个方法会自动获取数组中的 id。
+    //关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids){
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断是否 关注了某用户
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
+
+
 }
